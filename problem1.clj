59d04c7952bf157d2fedc616782062f8195fbc20
Rafik Rezzik <rafik.rezzik@gmail.com>
2011-04-03 23:06:54 -0400

    Few changes in the function to use args instead of literals and added printing of the solution

--- problem1.clj
+++ problem1.clj
@@ -1,6 +1,7 @@
 (defn sum_of_multiples
   "Project euler #1: Find the sum of all multiples of 3 and 5 under 1000"
   [start limit x y]
-  (reduce + (distinct (concat (range 0 limit x) (range 0 limit y)))))
+  (reduce + (distinct (concat (range start limit x) (range start limit y)))))
 
-(sum_of_multiple 1000 5 3)
+; print the solution
+(println (sum_of_multiples 0 1000 5 3))
