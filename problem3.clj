622caf436ed8194de6607d3b205de165ca7902d8
Rafik Rezzik <rafik@zeus.(none)>
2011-04-09 21:33:19 -0400

    Added better primacy function and println the answer rather than print

--- problem3.clj
+++ problem3.clj
@@ -1,17 +1,9 @@
 (ns problem3
   (:use [clojure.contrib.math :only (sqrt)]))
 
-(defn is-prime?
-  "Check if a number is prime, probably not the most efficient but it works"
-  [x]
-  (let [limit (int (sqrt x))]
-    (loop [i 2]
-      (if (= i limit)
-	true
-	(if (zero? (rem x i))
-	  false
-	  (recur (inc i)))))))
-      
+(defn is-prime? [n]
+  "Checks whether the natural number n is prime."
+  (.isProbablePrime (bigint n) 10))
 
 (defn largest-prime-fact
   "Determines the largest prime factor of a number n"
@@ -24,4 +16,4 @@
 
 
 ;; solve the problem for the number 600851475143 and print it
-(print (largest-prime-fact 600851475143))
+(println (largest-prime-fact 600851475143))
